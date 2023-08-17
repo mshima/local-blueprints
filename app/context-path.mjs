c1eeef9c9e5bc726887c8a0645b2d21e8cb687be
Marcelo Shima <marceloshima@gmail.com>
2023-08-17 10:06:14 -0300

    adjusts to context-path

--- app/context-path.mjs
+++ app/context-path.mjs
@@ -34,7 +34,7 @@ export const createGenerator = async env => {
 
     get [BaseApplication.POST_WRITING]() {
       return this.asPostWritingTaskGroup({
-        async applyingContextPath({ application }) {
+        async applyContextPathToAngular({ application }) {
           this.log.info('Applying context-path');
           if (application.clientFrameworkAngular) {
             this.editFile('angular.json', content => {
@@ -69,18 +69,58 @@ export const createGenerator = async env => {
                 .replace('context: serverResources,', 'context: serverResources.map(res => `${contextPath}${res}`),'),
             );
           }
+        },
+
+        async applyContextPathToCommon({ application }) {
           if (!application.skipClient) {
             this.editFile('src/main/webapp/swagger-ui/index.html', content =>
               content.replace('<base href="/swagger-ui/"', `<base href="${this.contextPath}/swagger-ui/"`),
             );
           }
+        },
+
+        async applyContextPathToServer({ application }) {
           if (!application.skipServer) {
-            this.editFile('src/main/resources/config/application.yml', content => `${content}
+            this.editFile(
+              'src/main/resources/config/application.yml',
+              content => `${content}
 ---
 server:
   servlet:
     context-path: ${this.contextPath}/
-`);
+`,
+            );
+          }
+        },
+
+        async applyContextPathToCypress({ application }) {
+          if (application.cypressTests) {
+            this.editFile('src/test/javascript/cypress/support/commands.ts', content =>
+              content.replace(
+                'declare global {',
+                `
+
+/* Make intercept baseUrl aware */
+Cypress.Commands.overwrite('intercept', (originalFn: any, ...args: any[]) => {
+  const baseUrl = Cypress.config('baseUrl');
+  if (typeof args[1] === 'string' && args[1].startsWith('/')) {
+    args[1] = \`\${baseUrl}\${args[1]}\`;
+  }
+  if (typeof args[0].url === 'string' && args[0].url.startsWith('/')) {
+    args[0].url = \`\${baseUrl}\${args[0].url}\`;
+  }
+  return originalFn(...args);
+});
+
+declare global {`,
+              ),
+            );
+            this.editFile('src/test/javascript/cypress/support/navbar.ts', content =>
+              content.replaceAll('href="/', `href="${this.contextPath}/`),
+            );
+            this.editFile('cypress.config.ts', content =>
+              content.replace(/baseUrl: 'http:\/\/localhost:(.*)\/'/, `baseUrl: 'http://localhost:$1${this.contextPath}/'`),
+            );
           }
         },
       });
